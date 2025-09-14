5431d9de71dcc93c6f33ddb6c89b6af81f5a7e05
kunalkatara12 <kunalkatara888@gmail.com>
2025-09-14 19:45:59 +0530

    / removed from fronend url

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: ["http://localhost:5173", "http://localhost:5174","https://news-chat.netlify.app/"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "http://localhost:5174","https://news-chat.netlify.app"], credentials: true }));
 const port = process.env.PORT || 8985;
 app.use("/api/v1", router);
 app.get("/", async (_: Request, res: Response) => {
